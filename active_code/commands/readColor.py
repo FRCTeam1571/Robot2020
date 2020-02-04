330b39e2256b425fcf0c999655ba2b3b8a39983a
FRCTeam1571 <calrobotics1571@gmail.com>
2020-02-03 17:12:18 -0800

    color sensor work

--- active_code/commands/readColor.py
+++ active_code/commands/readColor.py
@@ -1,9 +1,14 @@
+import wpilib
 from wpilib.command import Command
-from subsystems import vision_sys
+from subsystems import vision_sys as vs
 
 class ColorSensorV3(Command):
     def __init__(self):
         Command.__init__(self)
+        self.totalRad = 0
+        self.totalRot = 0
         
+    # Function to read the starting color of the color panel
     def readcolor(self):
+        wpilib.SmartDashboard.putNumber("Proximity", vs.getProx)
         
