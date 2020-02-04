6ad39835abad6176a838006a71a16ec307849a81
FRCTeam1571 <calrobotics1571@gmail.com>
2020-02-03 18:03:12 -0800

    yollaine gyroscope additions

--- active_code/subsystems/gyroscope.py
+++ active_code/subsystems/gyroscope.py
@@ -7,6 +7,14 @@ from adi.ADIS16470_IMU import ADIS16470_IMU
 
 class Gyroscope(Subsystem):
     def __init__(self):
+        # Single Time Initialization
         Subsystem.__init__(self)
+        #
         self.imu = ADIS16470_IMU()
-        
+        # Axis Values
+        self.kYawDefault = 0.0 # Z-Axis
+        self.kYawXAxis = 0.0
+        self.kYawYAxis = 0.0
+
+        self.runCal =
+
