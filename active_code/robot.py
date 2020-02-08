8cec9463028165454ea5071f291c3311e3caf229
FRCTeam1571 <calrobotics1571@gmail.com>
2020-02-07 19:53:37 -0800

    yollaine gyroscope planning
    added more things and decided on a plan

--- active_code/robot.py
+++ active_code/robot.py
@@ -4,16 +4,18 @@ from commandbased import CommandBasedRobot
 from subsystems.DriveTrain import DriveTrain
 from subsystems.MasterController import MasterController
 from commands import drive
+from adis16470 import ADIS16470_IMU
+from adis16470 import ADIS16470CalibrationTime
 
 class Robot(CommandBasedRobot):
     ''' Statement of commands '''
     def robotInit(self):
-        self.controller = MasterController()
-        self.driveTrain = DriveTrain()
+        # self.controller = MasterController()
+        # self.driveTrain = DriveTrain()
 
-        Command.getRobot = lambda x=0: self
-
-        
+        # Command.getRobot = lambda x=0: self
+        self.gyro = ADIS16470_IMU()
+        self.m_imu.GetAngle()
 
     def autonomousInit(self):
         # add later
