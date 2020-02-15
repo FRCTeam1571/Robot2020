4b6ea868401e39e3dfb467692095ead17ff12f5c
FRCTeam1571 <calrobotics1571@gmail.com>
2020-02-14 21:21:51 -0800

    Hunk Repair

--- active_code/subsystems/ColorSpinner.py
+++ active_code/subsystems/ColorSpinner.py
@@ -9,15 +9,7 @@ class ColorSpinner(Subsystem):
         self.spinSpeed = 0
         self.motor = wpilib.NidecBrushless(0, 0)# PMW and DIO pins
         self.motor.setExpiration(0.1)
-<<<<<<< Updated upstream
 
-=======
-
-
-    def engageMotor(self, spinSpeed = 0.5):
-        self.motor.setSafetyEnabled(True)
-        self.motor.set(spinSpeed)
->>>>>>> Stashed changes
 
 
     def engageMotor(self, spinSpeed = 0.5):
